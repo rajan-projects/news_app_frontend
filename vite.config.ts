5adc4a5b13b8639f21017e658893bb6004de574f
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2025-02-15 14:52:18 +0530

    build 1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/news_app_frontend/',  // Repository name
+  build: {
+    outDir: './docs',
+    emptyOutDir: true
+  }
 })
