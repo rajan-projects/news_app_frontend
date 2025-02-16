4bdb2ac2bc1df5061de7411ab2e1386922b0a9ac
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2025-02-16 22:24:24 +0530

    API_URL in env

--- src/config/encore.ts
+++ src/config/encore.ts
@@ -1,12 +1,13 @@
 import Client from '../lib/client';
-// import { Environment, Local } from '../lib/client';
+import { Environment, Local } from '../lib/client';
 
 // Create a singleton instance of the Encore client
 const client = new Client(
-  "https://octopus-app-go4mu.ondigitalocean.app"
-  // process.env.NODE_ENV === 'production'
-  //   ? Environment('staging')
-  //   : Local
+  import.meta.env.VITE_ENCORE_API_URL || (
+    process.env.NODE_ENV === 'production'
+      ? Environment('staging')
+      : Local
+  )
 );
 
 export default client;
